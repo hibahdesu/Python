c64ed036257ed397f97c17b1535e42f770bbb802
hibahdesu <92212557+hibahdesu@users.noreply.github.com>
2023-08-06 17:49:41 +0300

    Update addFun.py

--- addFun.py
+++ addFun.py
@@ -3,10 +3,25 @@
 def add(num1, num2, num3):
 
     if type(num1) != int or type(num2) != int or type(num3) != int:
+        
         print("Please enter only integers")
 
     else:
+        
         print(num1 + num2 + num3)
 
 add(9, 39, 19)
+
+
+#Add new employees on the company with Fun
+
+def new_emp(*emp):
+
+    for new in emp: 
+
+        print(f'Hello {new} To Our Company, Happy to Have You Here.')
+
+
+new_emp('Jose', 'Jane', 'Mary', "Joseph")
+
 
