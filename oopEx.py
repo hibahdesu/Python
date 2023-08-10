a9ed8ba38f0185e998fcab6eb5ce344d557279ef
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2023-08-10 17:08:00 +0300

    Update oopEx.py

--- oopEx.py
+++ oopEx.py
@@ -23,3 +23,74 @@ print(f'The user name is: {user_two.name} {user_two.last}, and his age is: {user
 
 print(f'The user name is: {user_three.name} {user_three.last}, and his age is: {user_three.age}')
 
+
+#Ex 2:
+#OOP Example
+
+#Class
+
+class Users:
+    def __init__(self, user_name, user_last, user_age, user_gender):
+
+        self.name = user_name
+
+        self.last = user_last
+
+        self.age = user_age
+
+        self.gender = user_gender
+
+    def full_name(self):
+
+        return f'{self.name} {self.last}'    
+    
+    def title(self):
+        
+        if  self.gender == 'Male':
+
+            return f'Hello Mr.{self.name}'
+        
+        elif self.gender == 'Female':
+
+            return f'Hello Mrs.{self.name}'
+        
+        else: 
+
+            return f'Hello {self.name}'
+        
+    def uage(self):
+
+        return f'{self.age} years old'    
+    
+    def full_details(self):
+
+        return f'{self.title()} your age {self.uage()} is '
+
+user_1 = Users('Jane', 'Johnson', 23, 'Male')
+
+user_2 = Users('David', 'Joseph', 29, 'Male')
+
+user_3 = Users('Tanaka', 'Naoki', 25, 'Male')   
+
+user_4 = Users('Sakura', 'Horishima', 26, 'Female')   
+
+print(user_1.full_name())
+
+print(user_1.title())
+
+print(user_4.full_name())
+
+print(user_4.title())
+
+print(user_1.uage())
+
+print(user_1.full_details())
+
+print(user_2.full_details())
+
+print(user_3.full_details())
+
+print(user_4.full_details())
+
+
+
