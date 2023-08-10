3fef7bee94707a07156afd7d8261269f3d18ae4d
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2023-08-10 15:27:34 +0300

    Update validation.py

--- validation.py
+++ validation.py
@@ -1,11 +1,39 @@
 #Validation
 import re
 
+#search()
 email = re.search(r'[A-z0-9\.]+@[A-z0-9]+\.(com|net)', 'hibo@hotmail.com')
 
 if email:
 
     print('Valid Email')
 
+    print(email.span())
+
+    print(email.string)
+
+    print(email.group())
+
 else: 
-    print('Unvalid Email')
+    print('Invalid Email')
+
+
+#findall()    
+userEmail = input('Type your email, Please ')
+
+emails = re.findall(r'[A-z0-9\.]+@[A-z0-9]+\.com|net', userEmail)
+
+users = []
+
+if emails != []:
+
+    users.append(emails)
+
+    print('Valid Email.\nAdded Succefuly')
+
+else:
+    print("Invalid Email")
+
+for email in users:
+
+    print(emails)    
